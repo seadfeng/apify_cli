29061b56a19ea5be7bb0dbd935e721cdc48dd723
Sead Feng <seadfeng@icoud.com>
2023-11-30 23:52:41 +0800

    Update test

--- spec/apify_client_spec.rb
+++ spec/apify_client_spec.rb
@@ -5,15 +5,29 @@ RSpec.describe Apify::Client do
     expect { Apify::Client.new }.to raise_error(ArgumentError, /Token cannot be nil/)
   end
 
-  it "Get 401 Test error" do
-    client = Apify::Client.new(token: 'test')
-    res = client.get_actors
-    expect(res['error'].is_a?(Hash)).to eq(true)
-  end
+  # it "Get 401 Test error" do
+  #   client = Apify::Client.new(token: 'test')
+  #   res = client.get_actors
+  #   expect(res['error'].is_a?(Hash)).to eq(true)
+  # end
 
   client = Apify::Client.new(token: ENV['APIFY_TOKEN'])
-  it "Get 200 Test" do
-    res = client.get_actors
-    expect(res['data']['total'].is_a?(Numeric)).to eq(true)
+  actors = client.get_actors
+  runs = client.get_runs
+
+  it "Get Actors" do
+    expect(actors['data']['total'].is_a?(Numeric)).to eq(true)
+  end
+
+  it "Get Runs" do
+    expect(runs['data']['total'].is_a?(Numeric)).to eq(true)
+  end
+
+  unless runs.nil? && runs['data']['items'].zero?
+    it "Get Run" do
+      first_run = runs['data']['items']&.first
+      run = client.get_run(id: first_run['id'])
+      expect(run['data']['status'].is_a?(String)).to eq(true)
+    end
   end
 end
